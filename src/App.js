e55757105957e5e262fd60d1b94542db7a0d736a
Pritam Gawade <pritamgawade@Pritams-MacBook-Pro.local>
2023-03-04 15:50:13 -0800

    react basics

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UserList";
+
+function App() {
+  const [userList, setUserList] = useState([]);
+
+  const addUserHandler = (userName, userAge) => {
+    setUserList((prevUserList) => {
+      return [
+        ...prevUserList,
+        { id: Math.random().toString(), name: userName, age: userAge },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={userList} />
+    </div>
+  );
+}
+
+export default App;
